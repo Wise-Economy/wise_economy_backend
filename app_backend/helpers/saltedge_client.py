8468c68f0e4e445e88847219df79a4276da18e47
Manoj Tutika <manojvenkat92@gmail.com>
2020-05-20 13:17:13 +0200

    Able to create customers in saltedge with the app users.

---
+++ app_backend/helpers/saltedge_client.py
@@ -0,0 +1,103 @@
+import OpenSSL.crypto as crypto
+import requests
+import base64
+import time
+import os
+
+
+class SaltEdge:
+    digest = "sha256"
+
+    @classmethod
+    def verify(cls, path_to_public_key, message, signature):
+        """
+        Verifies the signature on a message.
+        :param path_to_public_key: string, Absolute or relative path to Spectre public key
+        :param message: string, The message to verify.
+        :param signature: string, The signature on the message.
+        :return:
+        """
+        x509 = crypto.X509()
+        with open(path_to_public_key, 'r') as public_key_data:
+            public_key = crypto.load_publickey(crypto.FILETYPE_PEM, public_key_data)
+        x509.set_pubkey(public_key)
+
+        try:
+            crypto.verify(x509, base64.b64decode(signature), message, cls.digest)
+            return True
+        except crypto.Error:
+            return False
+
+    def __init__(self, app_id, secret, private_path):
+        self.app_id = app_id
+        self.secret = secret
+
+        with open(private_path, "rb") as private_key:
+            keydata = private_key.read()
+
+        self._private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, keydata)
+
+    def sign(self, message):
+        """
+        Signs a message.
+        :param message: string, Message to be signed.
+        :return: string, The signature of the message for the given key.
+          """
+        return base64.b64encode(crypto.sign(self._private_key, message, self.digest))
+
+    def generate_signature(self, method, expire, some_url, payload=""):
+        """
+        Generates base64 encoded SHA256 signature of the string given params, signed with the client's private key.
+        :param method: uppercase method of the HTTP request. Example: GET, POST, PATCH, PUT, DELETE, etc.;
+        :param expire: request expiration time as a UNIX timestamp in UTC timezone. Recommended value is 1 minute from now. The maximum value is 1 hour from now.
+        :param some_url: the full requested URL, with all its complementary parameters;
+        :param payload: the request post body. Should be left empty if it is a GET request, or the body is empty;
+        :return: base64 encoded SHA1 signature
+        """
+        message = "{expire}|{method}|{some_url}|{payload}".format(**locals())
+        return self.sign(message)
+
+    def generate_headers(self, expire):
+        return {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+            'Expires-at': expire,
+            'App-id': self.app_id,
+            'Secret': self.secret
+        }
+
+    def expires_at(self):
+        return str(time.time() + 60)
+
+    def get(self, some_url):
+        expire = self.expires_at()
+        headers = self.generate_headers(expire)
+        headers['Signature'] = self.generate_signature("GET", expire, some_url)
+        return requests.get(some_url, headers=headers)
+
+    def post(self, some_url, payload):
+        expire = self.expires_at()
+        headers = self.generate_headers(expire)
+        headers['Signature'] = self.generate_signature("POST", expire, some_url, payload)
+        return requests.post(some_url, data=payload, headers=headers)
+
+    def put(self, some_url, payload):
+        expire = self.expires_at()
+        headers = self.generate_headers(expire)
+        headers['Signature'] = self.generate_signature("POST", expire, some_url, payload)
+        return requests.put(some_url, data=payload, headers=headers)
+
+    def delete(self, some_url, payload):
+        expire = self.expires_at()
+        headers = self.generate_headers(expire)
+        headers['Signature'] = self.generate_signature("DELETE", expire, some_url, payload)
+        return requests.delete(some_url, data=payload, headers=headers)
+
+
+def initiate_saltedge_client():
+    saltedge_client = SaltEdge(
+        os.environ.get("APP_ID"),
+        os.environ.get("SECRET"),
+        os.environ.get("PRIVATE_SE_PEM_FILE_PATH"),
+    )
+    return saltedge_client
--- app_backend/models/users.py
+++ app_backend/models/users.py
@@ -1,17 +1,33 @@
+import json
+
 from django.contrib.auth.models import User
 from django.db import models
-
+from app_backend.helpers.saltedge_client import initiate_saltedge_client
 from app_backend.models import BankCustomerInfo
 from app_backend.models.banking import Country, BankProvider
+from app_backend.helpers.saltedge_urls import *
 
 
 class AppUser(User):
+
     se_customer_id = models.IntegerField(default=None, blank=True, null=True)
     se_identifier = models.CharField(default=None, blank=True, null=True, max_length=100)
     se_customer_type = models.CharField(default=None, blank=True, null=True, max_length=100)
     created_at = models.DateTimeField()
     last_updated = models.DateTimeField()
     country_id = models.ForeignKey(Country, default=None, blank=True, null=True, on_delete=models.CASCADE)
+    se_customer_secret = models.CharField(default=None, blank=True, null=True, max_length=200)
+
+    def create_saltedge_user_record(self):
+        client = initiate_saltedge_client()
+        payload = json.dumps({'data': {'identifier': self.email}})
+        response = client.post(CREATE_SALTEDGE_USER_ACCOUNT_URL, payload)
+        se_data = response.json()['data']
+        self.se_customer_id = int(se_data['id'])
+        self.se_identifier = se_data['identifier']
+        self.se_customer_secret = se_data['secret']
+        self.save()
+        return True
 
 
 class UserConnection(models.Model):
@@ -30,6 +46,6 @@ class UserConnection(models.Model):
     se_next_refresh_at = models.DateTimeField()
     se_last_success_at = models.DateTimeField()
     se_provider_id = models.IntegerField()
-    se_secret = models.CharField(max_length=200)
+    se_connection_secret = models.CharField(max_length=200)
     se_categorization = models.CharField(max_length=200)
     country_code = models.CharField(max_length=10)
