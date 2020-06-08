76f72c1f91dc037d8efb59ceddb0d090e325b892
Manoj Tutika <manojvenkat92@gmail.com>
2020-06-08 11:41:57 +0200

    Setup.sh modified.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 # put public.pem & private.pem in this folder
 # Add APP_ID, SECRET from saltedge into your ~/.bashrc
 # Add PRIVATE_SE_PEM_FILE_PATH to your ~/.bashrc
-# ADD PUBLIC_SE_PEM_FILE_PATH to your ~/.bashrc
 # Install python3
 # Install pip3
 
@@ -11,6 +10,10 @@ virtualenv -p python3 venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 source ~/.bashrc
+# set up DB.
+# Install postgresql.
+# CREATE DATABASE wise_economy;
+# GRANT ALL PRIVILEGES ON DATABASE "wise_economy" to manojt;
 python manage.py migrate
 ./manage.py shell < ./scripts/populate_countries.py
 python manage.py runserver
