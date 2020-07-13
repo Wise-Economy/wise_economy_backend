9b682e7e7bc8c41853a9b4bc8472d4d19c47f142
Manoj Tutika <manojvenkat92@gmail.com>
2020-07-13 13:10:38 +0200

    Authenticating the user right for the bank linking flow.

--- app_backend/views/global_home_screen.py
+++ app_backend/views/global_home_screen.py
@@ -1,17 +1,22 @@
 from django.http import JsonResponse
 from app_backend.models.user import AppUser
 from django.http import HttpResponseForbidden, HttpResponseServerError
-from app_backend.models.country import Country
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.contrib import auth
+from app_backend.models.user import USER_DEFAULTS
 
 
 @csrf_exempt
 def get_countries_linkable_for_homescreen(request):
     user = request.user
+    auth.authenticate(
+        request=request,
+        username=user.email,
+        password=USER_DEFAULTS.DEFAULT_PASSWORD,
+    )
     if user.is_authenticated:
         app_user = AppUser.get_by_user(user)
-
         countries_linkable = [
             {
                 "country_id": app_user.resident_country.id,
